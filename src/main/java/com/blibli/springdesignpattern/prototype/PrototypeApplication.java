a59a0b39ace17ce54d886271dd131ccaf4197e69
bagasimmanuel <bagasimmanuel00@gmail.com>
2021-01-28 11:34:23 +0700

    Prototype lanjutan

--- src/main/java/com/blibli/springdesignpattern/prototype/PrototypeApplication.java
+++ src/main/java/com/blibli/springdesignpattern/prototype/PrototypeApplication.java
@@ -27,10 +27,16 @@ public class PrototypeApplication {
 
     @SpringBootApplication
     public static class Configuration{
-        @Bean
+        @Bean("standardCustomer")
         @Scope("prototype") // Membuat banyak Object
-        public Customer standartCustomer(){
+        public Customer standardCustomer(){
             return Customer.builder().category("STANDARD").discount(10).build();
+        }
+
+        @Bean("premiumCustomer")
+        @Scope("prototype") // Kalau ada 2 Tipe
+        public Customer premiumCustomer(){
+            return Customer.builder().category("VVVVIP").discount(10).build();
         }
     }
 
@@ -43,11 +49,14 @@ public class PrototypeApplication {
 
         // Cara cepat wkwk
         ApplicationContext context = SpringApplication.run(Configuration.class);
-        Customer customer1 = context.getBean(Customer.class);
-        Customer customer4 = context.getBean(Customer.class);
-        Customer customer3 = context.getBean(Customer.class);
+//        Customer customer1 = context.getBean(Customer.class); // Kalau di run sekarang error karena ada 2 prototype
+        Customer customer1 = context.getBean("standardCustomer",Customer.class);
+
+        Customer customer4 = context.getBean("premiumCustomer",Customer.class);
 
         System.out.println(customer1 == customer4); // False karena Objek baru :)
+        System.out.println(customer1);
+        System.out.println(customer4);
     }
 
 }
