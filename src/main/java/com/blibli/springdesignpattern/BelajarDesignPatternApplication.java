62075ab8eede68b754fab76da58268c3ea4ea8c8
bagasimmanuel <bagasimmanuel00@gmail.com>
2021-01-28 10:10:37 +0700

    Initial Commit
    Generate Template Spring

---
+++ src/main/java/com/blibli/springdesignpattern/BelajarDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.blibli.springdesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarDesignPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarDesignPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blibli/springdesignpattern/BelajarDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blibli.springdesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarDesignPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
