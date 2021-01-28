02f063a4702905bf97332e7647c6ebe92de6d57b
bagasimmanuel <bagasimmanuel00@gmail.com>
2021-01-28 14:36:39 +0700

    Chain of Responsibility

---
+++ src/main/java/com/blibli/springdesignpattern/chain/ChainApplication.java
@@ -0,0 +1,70 @@
+package com.blibli.springdesignpattern.chain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.graalvm.compiler.lir.CompositeValue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ChainApplication {
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Contoh{
+        private String name;
+
+    }
+
+    @SpringBootApplication
+    public static class HelloController(){
+
+        @GetMapping(value="/",produces = MediaType.APPLICATION_JSON_VALUE)
+        public Contoh helloWorld(){
+            return Contoh.builder().name("Hello World").build();
+        }
+    }
+
+    public static class AuthInterceptor implements HandlerInterceptor{
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            String xApi = request.getHeader("x-API");
+            if(StringUtils.hasText(xApi)){
+                return true;
+            }else{
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return false;
+            }
+        }
+    }
+
+
+    public static class WebConfiguration implements WebMvcConfigurer{
+
+        @Override
+        public void addInterceptors(InterceptorRegistry registry) {
+            registry.addInterceptor(new AuthInterceptor());
+        }
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChainApplication.class);
+    }
+
+
+
+}
